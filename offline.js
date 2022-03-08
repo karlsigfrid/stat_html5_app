84102bb458d2936de362a597521a7ba7a8ce2810
karlsigfrid <karlsigfrid@gmail.com>
2022-03-08 20:39:29 +0100

    Adding version 1

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1646768339,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/grid_tile-sheet0.png",
+		"images/plus_minus-sheet0.png",
+		"images/plus_minus-sheet1.png",
+		"images/professor-sheet0.png",
+		"images/professor-sheet1.png",
+		"images/professor-sheet2.png",
+		"images/sprite-sheet0.png",
+		"images/speech-sheet0.png",
+		"images/spritefont_a.png",
+		"images/spritefont_a2.png",
+		"images/grid_tile2-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
